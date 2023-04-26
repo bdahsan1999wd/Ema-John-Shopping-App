0203ec036368e3ffff87d6bec2e03906530a709e
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-04-26 16:08:19 +0600

    Order Summary Total Price added

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = ({ cart }) => {
+    // const cart = props.cart;  // option-1
+    // const {cart} = props;  // option-2
+
+    // console.log(cart);
+
+    let total = 0;
+
+    for(const product of cart){
+        total = total + product.price;
+    }
+
+    return (
+        <div className='cart'>
+            <h2>Order Summary</h2>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: $ </p>
+            <p>Tax: $ </p>
+            <h4>Grand Total: $ </h4>
+        </div>
+    );
+};
+
+export default Cart;
